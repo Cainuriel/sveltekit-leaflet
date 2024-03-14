fd3a522269fdb63bc4017e08eb283fee643abde8
Yuri <ygonzoo@gmail.com>
2024-03-14 03:06:08 +0100

    add example of popup with dynamic data
    - remove initial example
    - add dispatcher in Marker
    - add markerLocations
    - add comments with an example of adding Markers over time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,78 @@
 <script lang="ts">
 	import '../app.css';
-	import type { LatLngExpression } from 'leaflet';
+	import type { LatLng, LatLngExpression } from 'leaflet';
 	import Leaflet from '$lib/Leaflet.svelte';
 	import Marker from '$lib/Marker.svelte';
 	import Popup from '$lib/Popup.svelte';
 
-	const initialView: LatLngExpression = [51.514244, 7.468429]; // Dortmund, Germany
-	const markerLocations: Array<LatLngExpression> = [
-		[51.513870009926, 7.473969975241] // ShipBit Office
+	const initialView: LatLngExpression = [40.4165, -3.70256];
+
+	const contenidoPopupDinamico = {
+		Madrid: 'Bienvenido a Madrid!',
+		Barcelona: 'Bienvenido a Barcelona',
+		Palma: 'Bienvenido a Palma',
+		London: 'Welcome to London',
+		Amsterdam: 'Welcome to Amsterdam',
+		'New York': 'Welcome to New York',
+		Roma: 'Welcome to Roma',
+		Paris: 'Welcome to Paris',
+		'Los Angeles': 'Welcome to Los Angeles',
+		Cairo: 'Welcome to Cairo',
+		Berlin: 'Welcome to Berlin',
+		Sydney: 'Welcome to Sydney',
+		Atlanta: 'Welcome to Atlanta'
+	};
+
+	let markerLocations = [
+		['Madrid', [40.4165, -3.70256]],
+		['Barcelona', [41.3879, 2.16992]],
+		['Palma', [39.5519, 2.7388]],
+		['London', [51.509865, -0.118092]],
+		['Amsterdam', [52.3676, 4.9041]],
+		['New York', [40.6413, -73.7781]],
+		['Roma', [41.8005, 12.2558]],
+		['Paris', [48.8566, 2.3522]],
+		['Los Angeles', [34.0522, -118.2437]],
+		['Cairo', [30.0444, 31.2357]],
+		['Berlin', [52.52, 13.405]],
+		['Sydney', [-33.8688, 151.2093]],
+		['Atlanta', [33.749, -84.388]]
 	];
+
+	// const randomCoords = () => {
+	// 	const lat = Math.random() * 180 - 90;
+	// 	const lng = Math.random() * 360 - 180;
+
+	// 	const latlng = [lat, lng] as LatLngExpression;
+	// 	return latlng;
+	// };
+
+	// const addMarker = () => {
+	// 	console.log('Adding marker');
+	// 	markerLocations.push(randomCoords());
+	// 	markerLocations = markerLocations;
+	// };
+
+	// setInterval(addMarker, 3000);
+
+	let contenidoDelPopup = 'lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+	// setInterval(() => (contenidoDelPopup += " " + Date.now().toString()), 3000);
+
+	const onMarkerClick = (e: any) => {
+		console.log('Marker clicked', e.detail);
+		// fetch con e.detail
+		contenidoDelPopup = contenidoPopupDinamico[e.detail];
+	};
 </script>
 
 <div class="w-full h-screen">
-	<Leaflet view={initialView} zoom={14}>
-		{#each markerLocations as latLng}
-			<Marker {latLng} width={40} height={40}>
-				<!-- ShipBit Icon -->
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					xml:space="preserve"
-					style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"
-					viewBox="0 0 45 40"
-				>
-					<path
-						d="m23.046 25.449 9.601 16.885H13.253l9.793-16.885ZM45 23.965H25.702l9.575 16.84L45 23.965ZM44.963 20.923 35.339 4.254l-9.668 16.669h19.292ZM32.771 2.618h-4.17L8.522 37.237l2.08 3.603L32.771 2.618ZM25.084 2.618H11.465L0 22.476l6.768 11.722 18.316-31.58Z"
-						style="fill:#e9204f;fill-rule:nonzero"
-						transform="translate(0 -2.618)"
-					/>
-				</svg>
-
-				<Popup
-					>Like & Subscribe! This is a very loooooooooooong title and it has many characters.</Popup
-				>
+	<Leaflet view={initialView} zoom={5}>
+		{#each markerLocations as [city, latLng]}
+			<Marker {latLng} {city} width={40} height={40} on:click={onMarkerClick}>
+				<div>🧡 {city}</div>
+				<Popup>
+					{contenidoDelPopup}
+				</Popup>
 			</Marker>
 		{/each}
 	</Leaflet>
